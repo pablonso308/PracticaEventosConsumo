22176eb7d9b04c8399713467761dc8582fce3fc3
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-10-16 20:41:51 +0200

    modificaciones firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
