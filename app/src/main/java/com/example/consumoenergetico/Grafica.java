79e9326ae641a6517d7de30c8ae4a9b347ce38bd
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-10-16 20:45:36 +0200

    modificaciones firebase

--- app/src/main/java/com/example/consumoenergetico/Grafica.java
+++ app/src/main/java/com/example/consumoenergetico/Grafica.java
@@ -1,14 +1,39 @@
 package com.example.consumoenergetico;
 
 import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Grafica extends AppCompatActivity {
+    private static final String TAG = "Grafica";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafica);
+
+        // Referencia a la base de datos de Firebase
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // Leer los datos de la base de datos
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Imprimir los datos en la consola
+                Log.d(TAG, "Database contents: " + dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar errores
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
     }
-
 }
-
